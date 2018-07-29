871fa3911b8a13b0a047028ba45f6f0ae364bc76
murfmk <murfmk1130@gmail.com>
2018-07-29 07:16:08 +0000

    Add a Static Pages controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render html: "Get, Pokemon!"
+    render html: "Get, Pokemon"
   end
 end
