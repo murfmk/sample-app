057ddd290231db82b8733eeaf09b9fd529e952ee
murfmk <murfmk1130@gmail.com>
2018-07-31 04:05:45 +0000

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  git  root 'static_pages#home'
+  get  root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
-  get 'static_pages/contact'
 end
