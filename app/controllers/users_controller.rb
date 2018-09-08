b7f0734b6cc470637de9e9d1af4a369bb58bb064
murfmk <murfmk1130@gmail.com>
2018-09-08 16:24:21 +0000

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def index
     @users = User.where(activated: true).paginate(page: params[:page])
   end
-
+ 
   def show
     @user = User.find(params[:id])
     redirect_to root_url and return unless @user.activated?
